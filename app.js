e333d584e3a0ae54be2c18c1a1c7c51982a4161c
Arthur Rosch <rosch.arthur@gmail.com>
2022-10-11 20:59:20 -0300

    Lista 34 finalizado

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
 // 1.Faça uma função que receba um parâmetro string e um segundo como string
 // também, a função deve retornar true caso o primeiro termine como o segundo.
-// function fim(frase: string, fim: string) {
-//   (frase = frase.toLowerCase()), (fim = fim.toLowerCase());
-//   let ultimaLetras = frase.substring(frase.length - fim.length);
-//   if (ultimaLetras == fim) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-// console.log(fim("Arthur", "HUR"));
+function fim(frase, fim) {
+    (frase = frase.toLowerCase()), (fim = fim.toLowerCase());
+    var ultimaLetras = frase.substring(frase.length - fim.length);
+    if (ultimaLetras == fim) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+console.log(fim("Arthur", "HUR"));
 // 2. Faça uma função que receba uma string como parâmetro, e deve converter para
 // uma nova string onde cada caractere deve ser substituído por “(“ se aparece
 // apenas uma vez na string original ou por “)” se aparecer mais de uma vez. A função
 // não deve diferenciar maiúsculas e minúsculas para fazer a contagem.
-// function Substituindo(frase: string): string {
-//   frase = frase.toLocaleLowerCase();
-//   let arrayLetras = frase.split("");
-//   for (let A = 0; A < arrayLetras.length; A++) {
-//     if (arrayLetras.includes(frase[A], A + 1)) {
-//       arrayLetras.splice(A, 1, ")");
-//       arrayLetras.splice(arrayLetras.indexOf(frase[A]), 1, ")");
-//     }
-//   }
-//   arrayLetras.forEach((item: string, index: number) => {
-//     if (item != ")") {
-//       arrayLetras[index] = "(";
-//     }
-//   });
-//   return arrayLetras.join("");
-// }
-// console.log(Substituindo("TECH"));
+function Substituindo(frase) {
+    frase = frase.toLocaleLowerCase();
+    var arrayLetras = frase.split("");
+    for (var A = 0; A < arrayLetras.length; A++) {
+        if (arrayLetras.includes(frase[A], A + 1)) {
+            arrayLetras.splice(A, 1, ")");
+            arrayLetras.splice(arrayLetras.indexOf(frase[A]), 1, ")");
+        }
+    }
+    arrayLetras.forEach(function (item, index) {
+        if (item != ")") {
+            arrayLetras[index] = "(";
+        }
+    });
+    return arrayLetras.join("");
+}
+console.log(Substituindo("TECH"));
 // 3. Faça uma função que recebe dois parâmetros: um número inteiro positivo ”n” e um
 // dígito “d” entre 0 e 9. A função deve calcular o quadrado de todos os números entre
 // 0 e “n”, e depois contar quantas vezes o dígito “d” aparece nos resultados das
